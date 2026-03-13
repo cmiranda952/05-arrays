c999da23f1be8f5d453c447602f8f70e351330cc
Carlos Miranda Jr <carlos.miranda2@my.utsa.edu>
2026-03-13 16:33:22 +0000

    Refactor movie selection logic to populate movie list based on genre selection

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
+  let movieList = [];
 
   // Add movies to the movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    movieList.push("Superbad", "The Mask", "Mean Girls", "Step Brothers");
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    movieList.push("Mad Max: Fury Road", "John Wick", "Die Hard", "The Dark Knight");
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    movieList.push("Forrest Gump", "The Pursuit of Happyness", "The Shawshank Redemption", "A Beautiful Mind");
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    movieList.push("Inception", "Interstellar", "The Matrix", "Blade Runner 2049");
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}!`;
 });
